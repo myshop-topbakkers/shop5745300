3e978d6a839c0d4b2698099cc2340bfe4e54d477
myshop-topbakkers <john@myshop.com>
2017-08-15 21:27:54 +0200

    added vue component

---
+++ pages/page29/block151/vue/src/myshop/Version.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="myshp-version">
+  Version: {{ version }}
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'version',
+  data: function () {
+    return {
+      version: '0.0.1'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.myshp-version {
+  color: #42b983;
+}
+</style>
