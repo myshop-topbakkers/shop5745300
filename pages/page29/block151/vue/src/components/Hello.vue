261dea0446eaf14f24da39171ae9d4c8edbfeb9a
John Zanoni <john.zanoni@myshop.com>
2017-08-16 15:07:00 +0200

    added simple list

--- pages/page29/block151/vue/src/components/Hello.vue
+++ pages/page29/block151/vue/src/components/Hello.vue
@@ -1,22 +1,9 @@
 <template>
   <div class="hello">
-    update
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2></h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    <li>item</li>
     </ul>
   </div>
 </template>
@@ -26,13 +13,12 @@ export default {
   name: 'hello',
   data: function () {
     return {
-      msg: 'Welcome to Top Bakkers Order List App'
+      msg: 'Top Bakkers Order List App'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
