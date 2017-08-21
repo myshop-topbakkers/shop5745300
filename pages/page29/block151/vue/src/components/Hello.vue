95693873d575d336db89b85c9337143eedce4b21
John Zanoni <john.zanoni@myshop.com>
2017-08-21 15:01:00 +0200

    Added order line remove

--- pages/page29/block151/vue/src/components/Hello.vue
+++ pages/page29/block151/vue/src/components/Hello.vue
@@ -15,43 +15,23 @@
                 <td class="myshp_basket_product_quantity">
                     <resource name="nr" filter="upper"></resource>
                 </td>
-                <td class="myshp_basket_product_unitprice">
-                    <resource name="price"></resource>
-                </td>
-                <td class="myshp_basket_product_totalprice">
-                  <resource name="total"></resource>
-                </td>
                 <td class="myshp_basket_product_controls">
                     <span>&nbsp;</span>
                 </td>
             </tr>
-            <tr class="myshp_basket_product" v-for="row in this.$store.state.basket.rows">
-                <td class="myshp_basket_product_name">
-                    <div v-if="''!=row.image">
+            <tr class="myshp_basket_product" v-for="(row, index) in this.$store.state.basket.rows">
+                <td class="myshp_basket_product_name"><div v-if="''!=row.image">
                       <img :src="row.image"/><br/>
-                    </div>
-                    {{row.id}}
+                    </div>{{row.id}}
                 </td>
                 <td class="myshp_basket_product_description" colspan="2">
                   <p>{{row.description}}</p>
                 </td>
                 <td class="myshp_basket_product_quantity">
-                    <input style="myshop-control:quantity-update" /><!-- value="myshop-value:quantity;" -->
+                    <p>{{row.quantity}}</p>
                 </td>
-                <td class="myshp_basket_product_unitprice">
-                    {{row.inc_vat_price_formatted}}
-                </td>
-                <td class="myshp_basket_product_totalprice">
-                    {{row.inc_vat_price_formatted}}
-                </td>
-                <td class="myshp_basket_product_controls" width="70">
-                    <a style="myshop-action:update-product" href="javascript:void(0);" class="myshp_button_update" data-role="button" data-icon="refresh" data-iconpos="notext" data-inline="true">
-                        <resource name="change"></resource>
-                    </a>&nbsp;
-    <a style="myshop-action:remove-product" href="javascript:void(0);" class="myshp_button_delete" data-role="button" data-icon="delete" data-iconpos="notext" data-inline="true">
-                  <resource name="remove"></resource>
-
-                    </a>
+                <td class="myshp_basket_product_controls" width="70px">
+                <remove :index="index" ></remove>
                 </td>
             </tr>
           </table>
@@ -65,11 +45,12 @@
 
 <script>
 import Resource from '../myshop/Resource'
+import Remove from '../myshop/OrderLineRemove'
 
 export default {
   name: 'hello',
   components: {
-    Resource
+    Resource, Remove
   },
   data: function () {
     console.log("Get the hello data");
---
+++ pages/page29/block151/vue/src/myshop/OrderLineRemove.vue
@@ -0,0 +1,31 @@
+<template>
+ <div v-html="label"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'remove',
+  props: ['name','index'],
+  data: function () {
+    return {
+    }
+  },
+  computed: {
+    label: function () {
+      return "<a data-inline='true' data-iconpos='notext' data-icon='delete' data-role='button' class='myshp_button_delete' href=''#'' onclick=\"return myshop().a('remove'," + (this.index + 1)+ "),false;\">"+this.$store.state.list['remove']+"</a>"
+    }
+  },
+  created: function () {
+    this.$store.commit('FETCH_RESOURCES');
+   }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.myshp-resource {
+  color: #42b983;
+}
+</style>
